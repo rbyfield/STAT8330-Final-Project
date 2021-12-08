82f41fd8c3b44672c984056baf983ee9d5581a51
sattarik <54645299+sattarik@users.noreply.github.com>
2021-12-07 22:22:51 -0600

    Update ExploratoryAnalysisCode_kia.R

--- ExploratoryAnalysisCode_kia.R
+++ ExploratoryAnalysisCode_kia.R
@@ -13,14 +13,44 @@ library(ggplot2) # package for plotting
 
 # reading the data
 nc_SST_orig <- nc_open('SSTdata_011948_022018.nc')
-nc_Prec_orig <- nc_open('Pdata_011948_022018-1.nc')
+nc_prec_orig <- nc_open('Pdata_011948_022018-1.nc')
 
+##### SST variables 
+zlev <- ncvar_get(nc_SST_orig, "zlev")
+# East Longitude (deg)
+X_SST <- ncvar_get(nc_SST_orig, "X")
+print (dim(X_SST))
+# time from Jan. 1948 to Feb. 2018 
+# 12*(2018-1948)+1(Jan 2018)+1(Feb 2018) = 842
+T_SST <- ncvar_get(nc_SST_orig, "T")
+print (dim(T_SST))
+# Latitude (deg)
+Y_SST <- ncvar_get(nc_SST_orig, "Y")
+print (dim(Y_SST))
+SST_anom <- ncvar_get(nc_SST_orig, "anom")
+print (dim(SST_anom))
+image(x=X_SST, y=Y_SST, z=SST_anom[,,842], 
+      col = hcl.colors(12, "YlOrRd", rev = TRUE),
+      xlab="East Longitude (deg)",
+      ylab="Latitude (deg)", main="SST Anomalies: Feb 2018")
 
-lon <- ncvar_get(nc_Prec_orig, "lon")
-lat <- ncvar_get(nc_Prec_orig, "lat", verbose = F)
-t <- ncvar_get(nc_Prec_orig, "time")
-
-head(lon) # look at the first few entries in the longitude vector
+##### precipitation variables
+# East Longitude (deg)
+X_prec <- ncvar_get(nc_prec_orig, "X")
+print (dim(X_prec))
+# time 
+T_prec <- ncvar_get(nc_prec_orig, "T")
+print (dim(T_prec))
+# Latitude (deg)
+Y_prec <- ncvar_get(nc_prec_orig, "Y")
+print (dim(Y_prec))
+# Precipitation
+prec <- ncvar_get(nc_prec_orig, "rain")
+# reproduce the image in file
+image(x=X_prec, y=Y_prec, z=prec[,,842], 
+      col = hcl.colors(12, "YlOrRd", rev = TRUE),
+      xlab="West Longitude (deg)",
+      ylab="Latitude (deg)", main="Precipitation: Feb 2018")
 
 # dimension reduction
 
@@ -29,3 +59,4 @@ head(lon) # look at the first few entries in the longitude vector
 
 
 # prediction
+
