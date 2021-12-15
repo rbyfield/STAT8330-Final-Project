93f9dc168d298545bc4cb9bdc31d084a0ee611f2
rbyfield <95316506+rbyfield@users.noreply.github.com>
2021-12-14 20:25:00 -0600

    Add files via upload

--- Regularization_Code.R
+++ Regularization_Code.R
@@ -3,6 +3,7 @@ Data<-read_excel('C:\\Users\\richa\\OneDrive\\Documents\\Final_Project_4C.xlsx')
 Data
 Data2 <- read_excel('C:\\Users\\richa\\OneDrive\\Documents\\Final_Project_Section_Split.xlsx')
 Data2
+typeof(Data2)
 
 Data$NA_Section <- as.factor(Data$NA_Section)
 Data$Pacific_Section <- as.factor(Data$Pacific_Section)
@@ -30,8 +31,6 @@ Data2$P_9 <- as.factor(Data2$P_9)
 Data2$P_10 <- as.factor(Data2$P_10)
 Data2$Pacific_Section <- as.factor(Data$Pacific_Section)
 Data2$Season <- as.factor(Data$Season)
-Data2$Month <- as.factor(Data$Month)
-Data2$Year <- as.factor(Data$Year)
 quantile(Data2$Average_Prec)
 
 
@@ -74,3 +73,61 @@ coef(model_cv)
 
 Reduced_Data <- Data2[, -which(names(Data2) %in% c('P_1', 'P_5', 'P_6'))]
 Reduced_Data
+
+set.seed(1)
+dt = sort(sample(nrow(Reduced_Data), nrow(Reduced_Data)*.7))
+train<-Reduced_Data[dt,]
+test<-Reduced_Data[-dt,]
+train
+test
+Prec.test <- test$Average_Prec
+
+library(tree)
+tree.1 <- tree(Average_Prec~., train)
+summary(tree.1)
+cv.tree1 <- cv.tree(tree.1)
+yhat <- predict(tree.1, newdata = test)
+mean((yhat - Prec.test)^2)
+
+library(randomForest)
+set.seed(1)
+mod_bag <- randomForest(Average_Prec~., train, mtry = 21)
+yhat.bag <- predict (mod_bag , newdata = test)
+mean((yhat.bag - Prec.test)^2)
+
+
+mod_RF <- randomForest(Average_Prec~., train, importance = TRUE)
+yhat.RF <- predict (mod_RF , newdata = test)
+mean((yhat.RF - Prec.test)^2)
+importance(mod_RF)
+varImpPlot(mod_RF)
+
+
+library(gbm)
+set.seed(1)
+mod_boost <- gbm(Average_Prec~., data = train,distribution = "gaussian" , n.trees = 5000,interaction.depth = 4)
+summary(mod_boost)
+yhat.boost <- predict (mod_boost , newdata = test)
+mean((yhat.boost - Prec.test)^2)
+
+typeof(Reduced_Data)
+Reduced_Data <- data.frame(Reduced_Data)
+
+
+library(BART)
+x <- Reduced_Data[,2:22]
+y <- Reduced_Data[,1]
+xtrain <- train[, 2:22]
+ytrain <- train[,1]
+xtest <- test[,2:22]
+ytest <- test[,1]
+xtrain = as.data.frame(xtrain)
+ytrain = as.matrix(ytrain)
+xtest = as.matrix(xtest)
+ytest = as.matrix(ytest)
+typeof(ytrain)
+nrow(xtrain)
+nrow(ytrain)
+
+set.seed(1)
+mod.bart <- gbart(train[,2:22], train[,1], x.test = test[,2:22])
